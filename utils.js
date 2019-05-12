83aac893e904f22383826e11e901a53c2ffb871b
Mikita Tsikhanovich <nikita.tihonovich@gmail.com>
2019-05-12 19:59:09 +0300

    Убран бесполезный метод explode

--- utils.js
+++ utils.js
@@ -1,34 +1,6 @@
 var Url = require('url');
 var fs = require('fs');
 
-String.prototype.explode = function(delimiter) {
-	var emptyArray = { 0: '' };
-
-	if (arguments.length != 2
-		|| typeof arguments[0] == 'undefined'
-		|| typeof arguments[1] == 'undefined') {
-		return null;
-	}
-
-	if ( delimiter === ''
-		|| delimiter === false
-		|| delimiter === null) {
-		return false;
-	}
-
-	if (typeof delimiter == 'function'
-		|| typeof delimiter == 'object'
-		|| typeof string == 'function'
-		|| typeof string == 'object') {
-		return emptyArray;
-	}
-
-	if (delimiter === true) {
-		delimiter = '1';
-	}
-	return this.toString().split(delimiter.toString());
-}
-
 module.exports = {
 	randomString: function(length) {
 		var id = "";
