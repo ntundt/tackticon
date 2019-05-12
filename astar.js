8d0da9702781e287e8040f121ad97c01542c0486
Mikita Tsikhanovich <nikita.tihonovich@gmail.com>
2019-05-12 19:56:16 +0300

    Профикшен баг: теперь стоимость перехода к клеткам пересчитывается нормально.

--- astar.js
+++ astar.js
@@ -5,10 +5,7 @@ module.exports = class AStar {
 		this.alreadyCalced = [];
 		this.calcQueue = [];
 		this.calcQueueIndex = 0;
-		this.costsTable = {
-			"g": 1,
-			"w": false
-		};
+		this.prices = {};
 	}
 	setStart(x, y) {
 		this.startX = x;
@@ -20,14 +17,35 @@ module.exports = class AStar {
 		this.endY = y;
 		this.costTable = [];
 	}
+	setPrices(prices) {
+		this.prices = prices;
+		this.costTable = [];
+	}
 	calculateMinimalCost() {
 		for (let i = 0; i < this.map.getXSize(); i++) {
 			this.costTable.push([]);
 			this.alreadyCalced.push([]);
 		}
-		this.calcQueue.push({ node: this.map.getNode(this.startX, this.startY), costMultiplier: 1});
+		this.calcQueue.push({ node: this.map.getNode(this.startX, this.startY), costMultiplier: 1 });
 		this.costTable[this.startX][this.startY] = 0;
-		this.calculateNeighboursCosts(this.startX, this.startY);
+
+		while (this.calcQueue[this.calcQueueIndex]) {
+			var nowCalculating = this.calcQueue[this.calcQueueIndex];
+			var neightbours = this.map.getAdjacentNodes(nowCalculating.node.x, nowCalculating.node.y);
+			for (let i = 0; i < neightbours.length; i++) {
+				if (this.prices[neightbours[i].node.type] * neightbours[i].costMultiplier + this.costTable[nowCalculating.node.x][nowCalculating.node.y] < this.costTable[neightbours[i].node.x][neightbours[i].node.y]) {
+					this.calcQueue.push(neightbours[i]);
+				}
+				if (this.prices[neightbours[i].node.type] * neightbours[i].costMultiplier + this.costTable[nowCalculating.node.x][nowCalculating.node.y] < this.costTable[neightbours[i].node.x][neightbours[i].node.y] || this.costTable[neightbours[i].node.x][neightbours[i].node.y] === undefined) {
+					this.costTable[neightbours[i].node.x][neightbours[i].node.y] = this.prices[neightbours[i].node.type] * neightbours[i].costMultiplier + this.costTable[nowCalculating.node.x][nowCalculating.node.y];
+				}
+				if (!this.alreadyInQueue(neightbours[i].node.x, neightbours[i].node.y) && this.prices[neightbours[i].node.type] !== false) {
+					this.calcQueue.push(neightbours[i]);
+				}
+			}
+			this.calcQueueIndex++;
+		}
+
 		return this.costTable[this.endX][this.endY];
 	}
 	alreadyInQueue(x, y) {
@@ -37,23 +55,5 @@ module.exports = class AStar {
 			}
 		}
 		return false;
-	}
-	calculateNeighboursCosts(x, y) {
-		var nowCalculating = this.calcQueue[this.calcQueueIndex];
-		var neightbours = this.map.getAdjacentNodes(nowCalculating.node.x, nowCalculating.node.y);
-		for (let i = 0; i < neightbours.length; i++) {
-			if (this.costsTable[neightbours[i].node.type] * neightbours[i].costMultiplier + this.costTable[x][y] < this.costTable[neightbours[i].node.x][neightbours[i].node.y] || this.costTable[neightbours[i].node.x][neightbours[i].node.y] === undefined) {
-				this.costTable[neightbours[i].node.x][neightbours[i].node.y] = this.costsTable[neightbours[i].node.type] * neightbours[i].costMultiplier + this.costTable[x][y];
-				// if (this.alreadyCalced[neightbours[i].node.x][neightbours[i].node.y] == undefined)
-				// 	this.calculateNeighboursCosts(neightbours[i].node.x, neightbours[i].node.y);
-			}
-			if (!this.alreadyInQueue(neightbours[i].node.x, neightbours[i].node.y) && this.costsTable[neightbours[i].node.type] !== false) {
-				this.calcQueue.push(neightbours[i]);
-			}
-		}
-		this.calcQueueIndex++;
-		var nextCalculating = this.calcQueue[this.calcQueueIndex];
-		if (nextCalculating !== undefined)
-			this.calculateNeighboursCosts(nextCalculating.node.x, nextCalculating.node.y);
 	}
 }
