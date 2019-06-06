ac240a818c24c09884f29c63a79591b210171d90
Mikita Tsikhanovich <nikita.tihonovich@gmail.com>
2019-06-06 17:47:01 +0300

    Теперь используем Express

--- main.js
+++ main.js
@@ -1,52 +1,57 @@
 var http = require("http");
 var fs = require("fs");
+var express = require("express");
+var app = express();
 
 var Node = require("./node.js");
 var Unit = require("./unit.js");
 var GameMap = require("./map.js");
 var Game = require("./game.js");
 var GamesList = require("./games_list.js");
-var Api = require("./api.js");
 var AStar = require("./astar.js");
+var Users = require("./users.js");
 
 var consts = require("./enum.js");
 var utils = require("./utils.js");
 var logger = require("./logger.js");
 
-var config = JSON.parse(fs.readFileSync("./config/config.json"));
+var settings = require("./file_loader.js").loadJson("./config/settings.json");
 
 var gamesListInstance = new GamesList();
+
+global.musthave = {
+	gamesListInstance,
+	logger,
+	Users
+};
+
+var Api = require("./api.js");
 var API = new Api(gamesListInstance);
 
-gamesListInstance.startNewGame({
-	mapOptions: "1.map", 
-	config: "attack.json"
+
+if (settings.enable_debug_messages) {
+	logger.enableDebug();
+}
+
+if (settings.enable_autosave) {
+	setInterval(function() {
+	 	logger.log("Saving running games…");
+	 	gamesListInstance.saveAll(function() {logger.log("Saved successfully")});
+	}, settings.autosave_interval * 1000);
+}
+
+app.use(express.static('markup'));
+
+app.get("/method/:method_name", (req, res) => {
+	res.set({
+		"Content-Type": "text/json"
+	})
+	try {
+		let response = API.callMethod(req.params.method_name, req.query);
+	} catch (e) {
+		console.log(e);
+	}
+	res.end(JSON.stringify(response));
 });
 
-// setInterval(function() {
-// 	logger.log("Saving…");
-// 	gamesListInstance.saveAll(function() {logger.log("Saved")});
-// }, 10000);
-
-http.createServer(function(request, response) {
-	var parameters = utils.getParameters(request.url);
-
-	var path = utils.getUrlFolders(request.url);
-	if (path[1] == "method")
-	if (path[2] != "") {
-		response.writeHead(200, {'Content-Type': 'text/json'});
-		response.end(JSON.stringify(API.callMethod(path[2], parameters)));
-	} else {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.end("Do not open the link! Paste it into the client.");
-	};
-	if (path[0] == '' && (path[1] == '' || path[1] == 'index.html'))
-		utils.showInterface(response);
-	var game = gamesListInstance.startNewGame({map: consts.MAP_RANDOM});
-
-	// var astar = new AStar(gamesListInstance.list[0].map);
-	// astar.setStart(0,0);
-	// astar.setEnd(6,6);
-	// response.writeHead(200, {'Content-Type': 'text/plain'});
-	// response.end(astar.calculateMinimalCost().toString());
-}).listen(8081);
+app.listen(settings.server_port);
