11c5a00cf80dadae5b5025f7b642b67af458f90d
Mikita Tsikhanovich <nikita.tihonovich@gmail.com>
2019-06-06 17:22:07 +0300

    Добавлен метод debug и опции к нему

--- logger.js
+++ logger.js
@@ -1,4 +1,13 @@
 module.exports = new (class Logger {
+	constructor() {
+		this.debugIsEnabled = false;
+	}
+	enableDebug() {
+		this.debugIsEnabled = true;
+	}
+	disableDebug() {
+		this.debugIsEnabled = false;
+	}
 	log(message) {
 		var date = new Date();
 		console.log(
@@ -7,4 +16,15 @@ module.exports = new (class Logger {
 			+ date.getMinutes() + ":" + date.getSeconds() + "] " + message
 		);
 	}
-});
+	debug(message) {
+		if (this.debugIsEnabled) {
+			var date = new Date();
+			console.log(
+				"\x1b[34m%s\x1b[0m",
+				"[" + date.getDate() + "." + date.getMonth() + "." 
+				+ date.getFullYear() + " " + date.getHours() + ":" 
+				+ date.getMinutes() + ":" + date.getSeconds() + "] " + message
+			);
+		}
+	}
+})();
